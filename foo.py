897f11daf8cc7d4c49baea3548c345b009e43fcd
Bharat Ramadas <70265814+Bharat-Ramadas@users.noreply.github.com>
2022-06-03 07:20:13 -0500

    Update foo.py

--- foo.py
+++ foo.py
@@ -26,3 +26,26 @@ class Foo:
     
     def __init__(self):
       pass
+
+class Foo2:
+    """This is the Foo2 class.
+    Real time doc update check.
+    
+    Create powerful Foo objects for generating foobars
+    Attributes
+    ----------
+    power (int): 
+        Amount of bar energy contained in this Foo instance.
+    Parameters
+    ----------
+    barPower : int
+        The bar energy to be contained in this Foo instance.
+    
+    Raises
+    ------
+    AssertError 
+        If barPower is less than 6, crap itself.
+    """
+    
+    def __init__(self):
+      pass
