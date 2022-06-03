63633b3281b0a63cbf132a627d426893498a109a
Bharat Ramadas <70265814+Bharat-Ramadas@users.noreply.github.com>
2022-06-03 08:06:42 -0500

    Update foo.py

--- foo.py
+++ foo.py
@@ -1,10 +1,10 @@
 """
-Autodocs trial
+Autodocs trial2
 """
 # autodocs fails on ReadTheDocs if I have any import statement like below"
 # from typing import Protocol, runtime_checkable, Any, List
 # from abc import abstractmethod, ABC
-# from copy import deepcopy
+from copy import deepcopy
 import os
 import sys
 
